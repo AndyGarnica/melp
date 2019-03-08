8d7c39dab533eb90091d1ff76bf0dfe1f3d15042
AndyGarey <elsa.garnica01@gmail.com>
2019-03-08 03:03:18 -0600

    tomando datos de array

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,35 @@
-const apiUrl = 'https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json';
+// Querys del DOM
 const list = document.querySelector('.list');
 
-fetch(apiUrl)
-    .then(function(resp){
-        return resp.json();
-    })
-    .then(function(contacts){
-        console.log(contacts);
-        printElements(contacts);
+// url de la API Datos de Restaurantes
+const apiUrlRest = 'https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json';
+
+fetch(apiUrlRest)
+    .then(function(response){
+        return response.json();
     })
 
 
-function printElements(contacts) {
-    for(let contact of contacts) {
+    .then(function(references){
+        console.log(references);
+        printElements(references);
+    })
+
+    .catch(function(error){
+        console.log(error)
+    })
+
+function printElements(references) {
+    for(let reference of references) {
         list.innerHTML += `
-            <li>${contact.name}</li>
+            <li class="list-rest">${reference.name}
+                <span>${reference.rating}</span>
+                <a>${reference.contact.site}</a>
+                <a>${reference.contact.email}</a>
+                <span>${reference.contact.phone}</span>
+                <p>${reference.address.street}</p>
+                <p>${reference.address.state}</p>
+            </li>
         `
     }
 }
